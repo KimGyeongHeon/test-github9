580e3265d789c96ea1bbc1f2e3469f915e51576e
KimGyeongHeon <kyasd658@naver.com>
2018-11-23 20:21:06 +0900

    곱하기 추가 했습니다

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,12 @@
 public class Calculator() {
 
-  public int sum(int a, int b) {
+  public int sum(int a, int b)
+  {
     return a + b;
   }
 
-  public int minus(int a, int b) {
+  public int minus(int a, int b)
+  {
     return a - b;
   }
 
